1961b75a9c4deb6f55b0ff39c8e5e94ad9261ac9
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-21 14:57:23 -0500

    solution

---
+++ code.js
@@ -0,0 +1,15 @@
+function favIceCream(flavor){
+  return "I love " + flavor;
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}

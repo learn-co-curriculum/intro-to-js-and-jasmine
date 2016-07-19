d7b23a33ab60c6383024fd0ade901c990b39eee4
Charles Pletcher <cpletcher2@gmail.com>
2016-07-19 17:07:01 -0400

    Move tests out of browser

--- spec/intro-spec.js
+++ spec/intro-spec.js
@@ -1,32 +1,38 @@
-'use strict';
-describe('Intro to Jasmine and JS basics', function() {
-  
-  describe('#favIceCream', function() {
-    it('should return your fav icecream', function() {
-      expect(favIceCream("mint chocolate chip")).toBe("I love mint chocolate chip");
-    });
-    
+const fs = require('fs');
+const jsdom = require('jsdom-global');
+const path = require('path');
+
+describe('Intro to Jasmine and JS basics', () => {
+  beforeAll(() => {
+    const src = fs.readFileSync(path.resolve(__dirname, '..', 'code.js'), 'utf-8');
+    this.jsdom = jsdom('<div></div>', { src });
   });
 
-  describe('#shouting', function() {
-    it('should return a string in all caps', function() {
-    
+  afterAll(() => {
+    this.jsdom();
+  });
+
+  describe('#favIceCream', () => {
+    it('should return your fav icecream', () => {
+      expect(favIceCream("mint chocolate chip")).toBe("I love mint chocolate chip");
+    });
+  });
+
+  describe('#shouting', () => {
+    it('should return a string in all caps', () => {
       expect(shouting("i love javascript soooooo much")).toBe('I LOVE JAVASCRIPT SOOOOOO MUCH');
     });
   });
 
-  describe('#roundDown', function() {
-    it('should correctly floor the number', function() {
+  describe('#roundDown', () => {
+    it('should correctly floor the number', () => {
       expect(roundDown(5.99)).toBe(5);
     });
   });
 
-  describe('#theTruth', function() {
-    it('return true', function() {
+  describe('#theTruth', () => {
+    it('return true', () => {
       expect(theTruth()).toBe(true);
     });
-    
   });
-
- 
 });
